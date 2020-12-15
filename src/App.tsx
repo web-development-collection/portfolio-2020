4affecf085ede72a30b928bc0a5f6d7263879f0e
ingoandelhofs <ingoandelhofs@gmail.com>
2020-12-15 11:14:48 +0100

    :art: Cleaned up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-const App = () => {
-  return <div/>;
+/**
+ * component App
+ * @author Ingo Andelhofs
+ */
+class App extends Component<any, any> {
+
+  public render() {
+    return <div>
+      <h1>Ingo Andelhofs</h1>
+    </div>;
+  }
 }
 
 
